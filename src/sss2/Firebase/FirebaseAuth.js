a2d2a0174705b564c1874d3b621895a939d8e4aa
projectmanagementsystem20 <projectmanagementsystem20@gmail.com>
2021-05-17 23:34:40 +0300

    BIGDELETE3

--- src/sss2/Firebase/FirebaseAuth.js
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-//$env:GOOGLE_APPLICATION_CREDENTIALS="D:\Azrieli\שנה ד\פרוייקט גמר\final_project_management_system\src\Firebase\project-sdk.json"
-
-admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://project-management-system-oz-default-rtdb.firebaseio.com/'
-  });
